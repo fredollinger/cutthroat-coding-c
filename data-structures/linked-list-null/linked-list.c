81718e9aa60129bb4997d62f369da6dd1b9c8f55
Frederick.Ollinger <follinge@gmail.com>
2024-10-07 15:21:28 -0700

    Add list_insert_after()

--- data-structures/linked-list-null/linked-list.c
+++ data-structures/linked-list-null/linked-list.c
@@ -4,10 +4,34 @@
 
 node* list_create() {
     node *head = malloc(sizeof(node));
+    node *z = malloc(sizeof(node));
+    head->next = z;
+    z->next = NULL;
     return head;
 }
+
+node* list_insert_after(node *ptr, int data) {
+    node *curr = ptr;
+    while(NULL != curr->next && data != curr->data) {
+        curr = curr->next;
+    }
+    node *neu = malloc(sizeof(node));
+    neu->data = data;
+    neu->next = curr->next;
+    curr->next = neu;
+    return neu;
+}
+
 
 int main()
 {
     node *head = list_create();
+    node *ptr = list_insert_after(head, 1);
+    ptr = list_insert_after(ptr, 2);
+    ptr = list_insert_after(ptr, 3);
+    ptr = list_insert_after(ptr, 4);
+    ptr = list_insert_after(ptr, 5);
+    ptr = list_insert_after(ptr, 6);
+    ptr = list_insert_after(ptr, 7);
+    ptr = list_insert_after(ptr, 8);
 }
--- data-structures/linked-list-null/linked-list.h
+++ data-structures/linked-list-null/linked-list.h
@@ -8,13 +8,14 @@ typedef struct node {
 
 /* linked list */
 node* list_create();
+node* list_insert_after(node *ptr, int data);
+
 void list_destroy(node **list);
-node* list_insert_after(node *node, void *data);
 node* list_insert_beginning(node *list, void *data);
 node* list_insert_end(node *list, void *data);
-void list_remove(node **list, node *node);
-void list_remove_by_data(node **list, void *data);
-node* list_find_node(node *list, node *node);
+void list_remove(node **list, node *ptr);
+void list_remove_by_data(node **list, void *ptr);
+node* list_find_node(node *list, node *ptr);
 node* list_find_by_data(node *list, void *data);
 node* list_find(node *list, int(*func)(node*,void*), void *data);
 
