8e24d978187d3b8dd78a24d4c0fac25654b627ed
Frederick.Ollinger <follinge@gmail.com>
2024-10-19 19:53:44 -0700

    Add list_insert_sorted()

--- data-structures/linked-list-null-2/list.c
+++ data-structures/linked-list-null-2/list.c
@@ -2,13 +2,16 @@
 
 // for malloc
 #include <stdlib.h>
+#include <stdio.h>
 
 node* list_create() {
     node *head = malloc(sizeof(node));
     node *tail = malloc(sizeof(node));
     head->next = tail;
+    head->key = -1000;
     tail->key = -1;
     tail->next = NULL;
+    return head;
 }
 
 node* list_insert_after(node *ptr, int key) {
@@ -32,18 +35,35 @@ void list_destroy(node *head) {
 node* list_insert_sorted(node *head, int key) {
     node *neu = malloc(sizeof(node));
     neu->key = key;
-    node *ptr = head->next; // skip past the head
-    while(ptr->key < key) {
+    node *ptr = head;
+    while(NULL != ptr->next->next) {
         // case where we have nothing inserted yet
-        if (ptr->next == NULL) {
-	    break;
-	}
+        if (ptr->next->key > key) {
+	        break;
+	    }
+        ptr = ptr->next;
     }
-    neu->next = ptr;
+    neu->next = ptr->next;
     ptr->next = neu;
     return neu;
 }
 
+node* print_list(node *head) {
+    node *ptr = head->next;
+    int i = 1;
+    while (NULL != ptr) {
+        printf("%i %p %i \n", i, ptr, ptr->key);
+	i++;
+	ptr = ptr->next;
+    }
+}
+
 int main() {
-    node *ll = list_create();
+    node *head = list_create();
+    node *ptr = list_insert_after(head, 71);
+    ptr = list_insert_sorted(head, 9);
+    ptr = list_insert_sorted(head, 1);
+    ptr = list_insert_sorted(head, 81);
+    // ptr = list_insert_sorted(head, 1);
+    print_list(head);
 }
