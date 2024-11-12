3fb21d0d18d3c671205417064be7e972b669e105
Frederick.Ollinger <follinge@gmail.com>
2024-11-12 09:38:10 -0800

    Add Sequential Search With Array

--- algorithms/binary-search/binary-search.c
+++ algorithms/binary-search/binary-search.c
@@ -1,3 +1,21 @@
+#include <stdio.h>
+
+// Given an item, find the spot where
+// that item appears in the list or return -1
+// if the item is not found
+int sequential_search(int item, int *arr, int len) {
+    printf("len: %i \n", len);
+    int i;
+    for (i=0; i < item; i++) {}
+    printf("search item: %i result: %i \n", item, i);
+}
+
 int main() {
+    int len = 100;
+    int list[len];
+    for (int i=0; i < len; i++) {
+        list[i] = i;
+    }
+    sequential_search(50, list, len);
 }
 
