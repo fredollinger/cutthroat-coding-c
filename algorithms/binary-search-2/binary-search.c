d9631f0117e127f952748c15f762d5574d448213
Frederick.Ollinger <follinge@gmail.com>
2024-11-21 20:32:41 -0800

    Implement Sequential Search

--- algorithms/binary-search-2/binary-search.c
+++ algorithms/binary-search-2/binary-search.c
@@ -2,12 +2,26 @@
 #include <assert.h>
 
 int sequential_search(int item, int *arr, int len) {
+    int c = 0;
+    int res = -1;
     for (int i = 0; i < len; i++) {
+	c = c + 1;
+        if (arr[i] == item) {
+	    res = arr[i];
+	    break;
+	}
+	else if (arr[i] > item) {
+	    break;
+	}
     }
-    return 50;
+    printf("sequential_search(): %i tries to find %i \n", c, res);
+    return res;
 }
 
-int binary_search(int item, int *arr, int len) {}
+int binary_search(int item, int *arr, int len) {
+    for(int i = 0; i < len; i++) {
+    }
+}
 
 void print_array(int *arr, int len) {
     for (int i = 0; i < len; i++) {
@@ -24,9 +38,14 @@ int main() {
         list[i] = prev + 5;
 	prev = list[i];
     }
-    print_array(list, len);
-    // int res1 = sequential_search(item, list, len);
-    // assert (res1 == item);
+    // print_array(list, len);
+    int res1 = sequential_search(item, list, len);
+    assert (res1 == item);
+    printf("item: %i result: %i \n", item, res1);
+    item = 51;
+    int res2 = sequential_search(item, list, len);
+    printf("item: %i result: %i \n", item, res2);
+    assert (res2 == item);
     /*
     printf("item: %i result: %i \n", item, res1);
     int res2 = binary_search(item, list, len);
