20e099be0a527c2011867e2497f37ff25b487ca4
Frederick.Ollinger <follinge@gmail.com>
2024-11-21 20:25:45 -0800

    Binary Search Start With Non-Consecutive List
    A non-consecutive list will test sequential and binary
    search better.

--- algorithms/binary-search-2/binary-search.c
+++ algorithms/binary-search-2/binary-search.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
+#include <assert.h>
+
+int sequential_search(int item, int *arr, int len) {
+    for (int i = 0; i < len; i++) {
+    }
+    return 50;
+}
+
+int binary_search(int item, int *arr, int len) {}
+
+void print_array(int *arr, int len) {
+    for (int i = 0; i < len; i++) {
+        printf("%i : %i \n", i, arr[i]);
+    }
+}
 
 int main() {
     int len = 100;
     int item = 50; // item to search for
     int list[len];
+    int prev = 0;
     for (int i=0; i < len; i++) {
-        list[i] = i;
+        list[i] = prev + 5;
+	prev = list[i];
     }
+    print_array(list, len);
+    // int res1 = sequential_search(item, list, len);
+    // assert (res1 == item);
     /*
-    int res1 = sequential_search(item, list, len);
     printf("item: %i result: %i \n", item, res1);
     int res2 = binary_search(item, list, len);
     printf("item: %i result: %i \n", item, res2);
