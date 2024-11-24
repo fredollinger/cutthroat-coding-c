333d32a8c0d02fc8cb1f5b795492666a5261bcf1
Frederick.Ollinger <follinge@gmail.com>
2024-11-24 13:48:49 -0800

    Add Binary Search Tree STUB

--- data-structures/binary-search-tree/binary-search-tree.c
+++ data-structures/binary-search-tree/binary-search-tree.c
@@ -1,6 +1,24 @@
-// #include <stdlib.h>
+#include <stdlib.h>  // malloc
 // #include <stdio.h>
 
+typedef struct node {
+  int key, info;
+  struct node *left;
+  struct node *right;
+} node;
+
+node* tree_initialize() {
+    node *head = (node*)malloc(sizeof(node));
+    node *z = (node*)malloc(sizeof(node));
+    z->left = z;
+    z->right = z;
+    z->info = -1;
+    head->left = z;
+    head->key = 0;
+    return head;
+}
+
 int main() {
+    node *head = tree_initialize();
     return 0;
 }
