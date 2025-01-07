51f9c508bdab77e4ee512f69c80a2e9e90ada4f0
Frederick Ollinger <follinge@gmail.com>
2025-01-06 16:08:14 -0800

    Add Binary Search Tree 2

---
+++ data-structures/binary-search-tree2/binary-search-tree.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>  // malloc
+#include <stdio.h>   // printf
+
+typedef struct node {
+  int key;
+  char info;
+  struct node *left;
+  struct node *right;
+} node;
+
+void tree_initialize(node **head, node **z) {
+}
+
+void tree_insert(node **head, node **z, int key) {
+}
+
+void tree_print(node *head, node *z) {
+}
+
+node* tree_search(node *head, node *z, int key) {
+}
+
+void tree_delete(node *head, node *z, int key) {
+    return;
+}
+
+int main() {
+   return 0;
+}
