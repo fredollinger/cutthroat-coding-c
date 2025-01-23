5a053a600739f009d29c749d2c47096b5e7476e3
Frederick Ollinger <follinge@gmail.com>
2025-01-23 13:23:29 -0800

    FizzBuzz Complete

--- easy/fizz-buzz/fizz-buzz.c
+++ easy/fizz-buzz/fizz-buzz.c
@@ -1,5 +1,20 @@
 #include <stdio.h>   // printf
 
 int main() {
-    return 0;
+    int line = 0;
+    for (int x=1; x <= 100; x++) {
+        line = 0;
+        if (x  % 3 == 0) {
+            printf("%i fizz ", x);
+            line = 1;
+        }
+        if (x  % 5 == 0) {
+            printf("%i buzz ", x);
+            line = 1;
+        } 
+        if (line) {
+            printf("\n");
+        }
+    }
+        return 0;
 }
