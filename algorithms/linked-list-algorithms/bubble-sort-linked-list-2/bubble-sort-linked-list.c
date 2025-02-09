7b489712e4ec3f513b9f308d9156776503bb76a0
Frederick Ollinger <follinge@gmail.com>
2025-02-09 11:07:05 -0800

    Bubble Sort 2 Linked List Complete

--- algorithms/linked-list-algorithms/bubble-sort-linked-list-2/bubble-sort-linked-list.c
+++ algorithms/linked-list-algorithms/bubble-sort-linked-list-2/bubble-sort-linked-list.c
@@ -1,11 +1,63 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct node {
+    int key;
+    struct node *next;
+} node;
+
+node* linked_list_initialize() {
+    node *head = malloc(sizeof(node));
+    head->next = NULL;
+    head->key = 0;
+    return head;
+}
+
+node* linked_list_insert(int key, node* ptr) {
+    node *neu = malloc(sizeof(node));
+    neu->next = ptr->next;
+    neu->key = key;
+    ptr->next = neu;
+    return neu;
+}
+
+void linked_list_print(node* head) {
+    node *ptr = head;
+    while(NULL != ptr) {
+        printf("%i \n", ptr->key);
+        ptr = ptr->next;
+    }
+}
+
+void linked_list_swap(node* ptr1, node *ptr2) {
+    int key = ptr1->key;
+    ptr1->key = ptr2->key;
+    ptr2->key = key;
+}
+
+node* linked_list_bubble_sort(node* head) {
+    node *ptr = head->next;
+    int done = 0;
+    while(!done){
+        done = 1;
+        ptr = head->next;
+        while(NULL != ptr->next) {
+            if (ptr->key > ptr->next->key) {
+                linked_list_swap(ptr, ptr->next);
+                done = 0;
+            }
+            ptr = ptr->next;
+        }
+    }
+    return head;
+}
+
 int main() {
     node *head = linked_list_initialize();
 
     // populate linked list
     node *ll = linked_list_insert(7, head);
+
     ll = linked_list_insert(6, ll);
     ll = linked_list_insert(5, ll);
     ll = linked_list_insert(4, ll);
@@ -14,14 +66,13 @@ int main() {
     ll = linked_list_insert(1, ll);
 
     printf("original \n");
-    print_linked_list(head);
+    linked_list_print(head);
 
-    node *two = linked_list_search(2, head);
     linked_list_bubble_sort(head);
 
     // linked_list_bubble_sort(head);
     printf("\n sorted \n");
-    print_linked_list(head);
+    linked_list_print(head);
 
     return 0;
 }
