3c01fcb9504267ce07b994ab3c6082c63c6472be
Frederick Ollinger <follinge@gmail.com>
2025-02-10 13:36:08 -0800

    Select Sort Linked List 3 Complete

---
+++ algorithms/linked-list-algorithms/select-sort-linked-list-3/select-sort-linked-list.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct node {
+    int key;
+    struct node *next;
+} node;
+
+node* linked_list_initialize() {
+    node *head = malloc(sizeof(node));
+    head->next = NULL;
+    head->key = 0;
+    return head;
+}
+
+node* linked_list_insert(int key, node* ptr) {
+    node *neu = malloc(sizeof(node));
+    neu->next = ptr->next;
+    neu->key = key;
+    ptr->next = neu;
+    return neu;
+}
+
+void linked_list_print(node* head) {
+    node *ptr = head;
+    while(NULL != ptr) {
+        printf("%i \n", ptr->key);
+        ptr = ptr->next;
+    }
+}
+
+node* linked_list_select_sort(node* head) {
+    node *ptr1 = head->next;
+    node *ptr2;
+    int key;
+    while(NULL != ptr1->next) {
+        ptr2 = ptr1;
+        while(NULL != ptr2) {
+            if(ptr1->key > ptr2->key) {
+                key = ptr1->key;
+                ptr1->key = ptr2->key;
+                ptr2->key = key;
+            }
+            ptr2 = ptr2->next;
+        }
+        ptr1 = ptr1->next;
+    }
+    return head;
+}
+
+int main() {
+    node *head = linked_list_initialize();
+
+    // populate linked list
+    node *ll = linked_list_insert(7, head);
+
+    ll = linked_list_insert(6, ll);
+    ll = linked_list_insert(5, ll);
+    ll = linked_list_insert(4, ll);
+    ll = linked_list_insert(3, ll);
+    ll = linked_list_insert(2, ll);
+    ll = linked_list_insert(1, ll);
+
+    printf("original \n");
+    linked_list_print(head);
+
+    linked_list_select_sort(head);
+
+    printf("\n");
+    printf("sorted \n");
+    linked_list_print(head);
+
+    return 0;
+}
