532477b139ca0563943ba15f0830561af0840454
Frederick Ollinger <follinge@gmail.com>
2025-03-27 14:11:24 -0700

    Insert Sort 2

--- algorithms/insert-sort-2/insert-sort.c
+++ algorithms/insert-sort-2/insert-sort.c
@@ -2,9 +2,28 @@
 #include <assert.h>
 
 void print_array(int *arr, int size) {
+    int i;
+    for (i = 0; i < size - 1; i++) {
+        printf("%i, ", arr[i]);
+    }
+    printf("%i \n", arr[size - 1]);
 }
 
 void insert_sort(int *arr, int size) {
+    int i, tmp, gap;
+    for (i = 1; i <= size; i++) {
+        gap = i - 1;
+        tmp = arr[gap];
+        while (gap >= 0) {
+            if (arr[gap] < tmp) {
+                break;
+            }
+            print_array(arr, size);
+            gap = gap - 1; 
+            arr[gap + 1] = arr[gap];
+        }
+        arr[gap + 1] = tmp;
+    }
 }
 
 int main() {
