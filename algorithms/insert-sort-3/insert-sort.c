31534589e847f5a8c95beb3ef32f06cb8b73d1c7
Frederick Ollinger <follinge@gmail.com>
2025-03-27 14:12:04 -0700

    Insert Sort 3 STUB

---
+++ algorithms/insert-sort-3/insert-sort.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <assert.h>
+
+void print_array(int *arr, int size) {
+}
+
+void insert_sort(int *arr, int size) {
+}
+
+int main() {
+    int arr[] = {7, 6, 5, 4, 3, 2, 1};
+    int size = 7;
+    print_array(arr, size);
+    insert_sort(arr, size);
+    print_array(arr, size);
+    printf("\n");
+    return 0;
+}
