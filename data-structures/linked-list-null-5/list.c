f7d45c965efaa8d8face4b308a13a53f1f9517fc
Frederick Ollinger <Frederick.Ollinger@hologic.com>
2026-04-01 15:29:39 -0700

    Implement list_create() 5

--- data-structures/linked-list-null-5/list.c
+++ data-structures/linked-list-null-5/list.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct node {
+typedef struct Node {
     int key;
-    struct node *next;
-} node;
+    struct Node *next;
+} Node;
 
 /* linked list */
-node* list_create();
+// node* list_create();
+/*
 node* list_insert_after(node *ptr, int key);
 void list_destroy(node *head);
 node* list_insert_end(node *head, int key);
@@ -16,6 +17,19 @@ void list_remove(node *head, node *ptr);
 void list_remove_by_key(node *head, int key);
 node* list_find_node(node *head, node *ptr);
 node* list_find_by_key(node *head, int key);
+*/
+
+Node* list_create() {
+    Node *head = (Node*)malloc(sizeof(Node));   
+    head->key = -1;
+    Node *tail = (Node*)malloc(sizeof(Node));   
+    tail->key = -2;
+    head->next = tail;
+    tail->next = NULL;
+    return head;
+}
 
 int main() {
+    Node *ptr = list_create();
+    printf("tail %i \n", ptr->next->key);
 }
