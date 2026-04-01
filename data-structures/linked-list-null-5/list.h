75b96da72a40e7e6551c068458699fac470126c1
Frederick Ollinger <Frederick.Ollinger@hologic.com>
2026-04-01 15:26:15 -0700

    Linked List NULL 5

---
+++ data-structures/linked-list-null-5/list.h
@@ -0,0 +1,20 @@
+#ifndef _LIST_H
+#define _LIST_H
+
+typedef struct node {
+	int key;
+	struct node *next;
+} node;
+
+/* linked list */
+node* list_create();
+node* list_insert_after(node *ptr, int key);
+void list_destroy(node *head);
+node* list_insert_end(node *head, int key);
+void list_remove(node *head, node *ptr);
+void list_remove_by_key(node *head, int key);
+node* list_find_node(node *head, node *ptr);
+node* list_find_by_key(node *head, int key);
+
+#endif
+
