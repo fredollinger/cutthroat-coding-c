474f7aa689faec906d9c548acb659bc3bf47c117
Frederick Ollinger <Frederick.Ollinger@hologic.com>
2026-04-03 17:01:02 -0700

    Linked List 5 Insert Before

--- data-structures/linked-list-null-5/list.c
+++ data-structures/linked-list-null-5/list.c
@@ -37,13 +37,43 @@ Node* linked_list_insert_after(Node *ptr, int key) {
     return neu;
 }
 
-void print_linked_list(Node *ll) {
+void linked_list_print(Node *ll) {
     Node *ptr = ll->next;
 
+    printf("linked_list_print() \n");
     while (NULL != ptr->next) {
         printf("key [%i] \n", ptr->key);
         ptr = ptr->next;
     }
+}
+
+// Naive approach, make a new list, and copy it over
+Node* linked_list_reverse_naive(Node *ll) {
+    Node *ptr = ll->next;
+    Node *head = linked_list_create();
+    Node *curr = head;
+  
+    while (NULL != ptr->next) {
+        // printf("key [%i] \n", ptr->key);
+        curr = linked_list_insert_after(curr, ptr->key);
+        ptr = ptr->next;
+    }
+    return head;
+}
+
+void linked_list_insert_before(Node *head, Node *before, int key) {
+    Node *ptr = head;
+    while (before != ptr->next) {
+        ptr = ptr->next;
+        printf("linked_list_insert_before [%i] \n", ptr->key);
+        // if (NULL == ptr->next->next) return;
+    }
+    Node *neu = (Node*)malloc(sizeof(Node));   
+    neu->key = key;
+    neu->next = ptr->next;
+    ptr->next = neu;
+    
+    return;
 }
 
 int main() {
@@ -54,10 +84,14 @@ int main() {
     ll = linked_list_insert_after(ll, 5);
     ll = linked_list_insert_after(ll, 4);
     ll = linked_list_insert_after(ll, 3);
-    ll = linked_list_insert_after(ll, 2);
     ll = linked_list_insert_after(ll, 1);
+    linked_list_insert_before(head, ll, 2);
 
-    print_linked_list(head);
+    linked_list_print(head);
+    
+    // Node *head2 = linked_list_reverse_naive(head);
+  
+    // linked_list_print(head2);
 
    return 0;
 }
