459a834be3e1b78cbda6d397558bd83c27b5f8c8
Frederick Ollinger <Frederick.Ollinger@hologic.com>
2026-08-18 11:40:13 -0700

    Binary Search Tree 2 Print Added

--- data-structures/binary-search-tree2/binary-search-tree.c
+++ data-structures/binary-search-tree2/binary-search-tree.c
@@ -1,29 +1,81 @@
 #include <stdlib.h>  // malloc
 #include <stdio.h>   // printf
 
+// A binary search tree is a rooted binary tree in which nodes are arranged in
+// strict total order in which the nodes with keys greater than any particular
+// node A is stored on the right sub-trees to that node A and the nodes with
+// keys equal to or less than A are stored on the left sub-trees to A,
+// satisfying the binary search property.
+
+
 typedef struct node {
   int key;
-  char info;
   struct node *left;
   struct node *right;
 } node;
 
-void tree_initialize(node **head, node **z) {
+void tree_initialize(node **head, node **end) {
+    *head = malloc(sizeof(node));
+    *end = malloc(sizeof(node));
+    (*head)->key = -1;
+    (*head)->left = *end;
+    (*head)->right = *end;
+    (*end)->left = NULL;
+    (*end)->right = NULL;
+    (*end)->key = -1;
 }
 
-void tree_insert(node **head, node **z, int key) {
+void tree_insert(node **head, node **end, int key) {
+    node *prev = *head;
+    node *ptr = (*head)->right;
+    // While we are not at the end
+    while (ptr != *end) {
+        if (key > ptr->key) {
+            printf("right \n");
+            ptr = ptr->right;
+        }
+        else {
+            printf("left \n");
+            ptr = ptr->left;
+        } 
+        prev = ptr;
+    }
+    node *neu = malloc(sizeof(neu));
+    neu->key = key;
+    neu->left = *end;
+    neu->right = *end;
+    if (key > prev->key) {
+        prev->right = neu;
+    }
+    else {
+        prev->left = neu;
+    }
 }
 
-void tree_print(node *head, node *z) {
+void tree_print(node *head, node *end) {
+    node *right = head->right;
+    node *left = head->left;
+    while (right != end && left != end) {
+        right = right->right;
+        left = left->left;
+        printf("right [%i] left [%i] \n", right->key, left->key);
+    }
 }
 
-node* tree_search(node *head, node *z, int key) {
+node* tree_search(node *head, node *end, int key) {
 }
 
-void tree_delete(node *head, node *z, int key) {
+void tree_delete(node *head, node *end, int key) {
     return;
 }
 
 int main() {
-   return 0;
+    node *head, *end; 
+    tree_initialize(&head, &end);
+    printf("head->key [%i] \n", head->key);
+    tree_insert(&head, &end, 10);
+    // printf("head->right [%i] \n", head->right->key);
+    // printf("head->left [%i] \n", head->left->key);
+    tree_print(head, end);
+    return 0;
 }
